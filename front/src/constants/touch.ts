15e4d8f8034f2560d4bf4e0066fb0d0dc337a188
jongmink0 <kjm010629@naver.com>
2025-11-07 17:22:22 +0900

    feat: 공통 HIT_SLOP 상수 추가
    - 컴포넌트 전반에서 일관된 터치 영역 확장을 위해 HIT_SLOP 상수 정의
    - SMALL, MEDIUM, LARGE, COMPACT, SAFE_VERTICAL, SAFE_HORIZONTAL 옵션 추가
    - 아이콘 및 버튼의 터치 영역 관리 용이성 향상

---
+++ front/src/constants/touch.ts
@@ -0,0 +1,13 @@
+export const HIT_SLOP = {
+  SMALL: { top: 8, bottom: 8, left: 8, right: 8 },
+
+  MEDIUM: { top: 10, bottom: 10, left: 10, right: 10 },
+
+  LARGE: { top: 12, bottom: 12, left: 12, right: 12 },
+
+  COMPACT: { top: 6, bottom: 6, left: 6, right: 6 },
+
+  SAFE_VERTICAL: { top: 4, bottom: 4, left: 8, right: 8 },
+
+  SAFE_HORIZONTAL: { top: 8, bottom: 8, left: 4, right: 4 },
+};
