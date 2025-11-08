f3b9b1e753d6eb8c75dcc0df07a263700cc2cca3
jongmink0 <kjm010629@naver.com>
2025-11-09 00:29:57 +0900

    feat: AuthStack 네비게이션 추가 및 RootNavigator 연동

---
+++ front/src/navigations/auth/AuthStack.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthScreen from '../../screens/auth/AuthScreen';
+import FindIdScreen from '../../screens/auth/FindIdScreen';
+
+export type AuthStackParamList = {
+  Login: undefined;
+  FindId: undefined;
+};
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
+
+export default function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={AuthScreen} />
+      <Stack.Screen name="FindId" component={FindIdScreen} />
+    </Stack.Navigator>
+  );
+}
