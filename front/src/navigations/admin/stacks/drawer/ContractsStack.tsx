5137f4928c80fe1768adeff4a6fc0b5d5245fb3a
jongmink0 <kjm010629@naver.com>
2025-11-10 00:36:19 +0900

    feat: 드로어 페이지 공통 AppHeader 적용으로 상단 헤더 구조 통일

--- front/src/navigations/admin/stacks/drawer/ContractsStack.tsx
+++ front/src/navigations/admin/stacks/drawer/ContractsStack.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { stackHeaderOptions } from '../../../common/headers';
 import ContractListScreen from '../../../../screens/admin/sidebar/ContractListScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function ContractsStack() {
   return (
-    <Stack.Navigator screenOptions={stackHeaderOptions}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="ContractsMain" component={ContractListScreen} />
     </Stack.Navigator>
   );
--- front/src/navigations/admin/stacks/drawer/ReservationsStack.tsx
+++ front/src/navigations/admin/stacks/drawer/ReservationsStack.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { stackHeaderOptions } from '../../../common/headers';
 import ReservationScreen from '../../../../screens/admin/sidebar/ReservationScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function ReservationsStack() {
   return (
-    <Stack.Navigator screenOptions={stackHeaderOptions}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="ReservationsMain" component={ReservationScreen} />
     </Stack.Navigator>
   );
