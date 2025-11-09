5072fa1ef718de3688b9dffee199174930e285af
jongmink0 <kjm010629@naver.com>
2025-11-09 16:40:38 +0900

    feat: 비밀번호 찾기 화면 라우트 및 이동 기능 추가

--- front/src/navigations/auth/AuthStack.tsx
+++ front/src/navigations/auth/AuthStack.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthScreen from '../../screens/auth/AuthScreen';
 import FindIdScreen from '../../screens/auth/FindIdScreen';
+import FindPwScreen from '../../screens/auth/FindPwScreen';
 
 export type AuthStackParamList = {
   Login: undefined;
   FindId: undefined;
+  FindPassword: undefined;
 };
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
@@ -15,6 +17,7 @@ export default function AuthStack() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={AuthScreen} />
       <Stack.Screen name="FindId" component={FindIdScreen} />
+      <Stack.Screen name="FindPassword" component={FindPwScreen} />
     </Stack.Navigator>
   );
 }
