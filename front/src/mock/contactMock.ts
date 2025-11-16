aa00d4a4a1583f24df54b03b28022dcf5b50f916
jongmink0 <kjm010629@naver.com>
2025-11-16 15:58:26 +0900

    feat: 사용자 문의하기 화면 및 스택 네비게이터 추가

---
+++ front/src/mock/contactMock.ts
@@ -0,0 +1,14 @@
+export const contactListMock = [
+  {
+    id: 1,
+    title: '담당팀장 | 배회차 관련 문의',
+    name: '서원섭',
+    phone: '010-1111-1111',
+  },
+  {
+    id: 2,
+    title: '총괄 매니저 | 앱 사용 및 정산금 관련 문의',
+    name: '서승동',
+    phone: '010-1111-1111',
+  },
+];
