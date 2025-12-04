2aec373a0ae277b6e6b90aa2614c721ac34eebd2
jongmink0 <kjm010629@naver.com>
2025-12-04 11:02:42 +0900

    feat(user-mypage): 사용자 마이페이지 API 연동

--- front/src/api/mypage.ts
+++ front/src/api/mypage.ts
@@ -1,16 +1,21 @@
 import axiosInstance from './axiosInstance';
 
-// 내 정보 조회
+// (관리자) 내 정보 조회
 export const getMyInfo = () => {
   return axiosInstance.get('/api/staffs/me');
 };
 
-// 등급별 지급 비율표
+// (사용자) 내 정보 조회
+export const getPartnerMyInfo = () => {
+  return axiosInstance.get('/api/partners/me');
+};
+
+// 등급별 지급 비율표 (관리자)
 export const getPartnerGrades = () => {
   return axiosInstance.get('/api/partner-grades');
 };
 
-// 차량관리 금액표
+// 차량관리 금액표 (관리자)
 export const getCarFeesByGrade = (gradeId: number) => {
   return axiosInstance.get(`/api/car-fees/${gradeId}`);
 };
