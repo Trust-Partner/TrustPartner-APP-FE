8ced719b516cc62a41abb497fd2baf208310b1f1
limgahyun <limgh2002@gmail.com>
2026-01-04 23:10:49 +0900

    refactor: hooks 파일 중복되는 refetch 설정제거

--- front/src/hooks/todo/useTodoFuelWashDetail.ts
+++ front/src/hooks/todo/useTodoFuelWashDetail.ts
@@ -1,18 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import {
-  getFuelWashDetail,
-  FuelWashDetailResponse,
-} from '../../api/todo';
+import { getFuelWashDetail, FuelWashDetailResponse } from '../../api/todo';
 
 export const useTodoFuelWashDetail = (locationId: number) => {
   return useQuery<FuelWashDetailResponse>({
     queryKey: ['todo', 'fuelWashDetail', locationId],
     queryFn: () => getFuelWashDetail(locationId),
     enabled: !!locationId,
-    staleTime: 0,
-    refetchOnMount: 'always',
-    refetchOnWindowFocus: true,
-    refetchOnReconnect: true,
   });
 };
-
--- front/src/hooks/todo/useTodoReturnDetail.ts
+++ front/src/hooks/todo/useTodoReturnDetail.ts
@@ -1,18 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import {
-  getReturnDetail,
-  ReturnDetailResponse,
-} from '../../api/todo';
+import { getReturnDetail, ReturnDetailResponse } from '../../api/todo';
 
 export const useTodoReturnDetail = (locationId: number) => {
   return useQuery<ReturnDetailResponse>({
     queryKey: ['todo', 'returnDetail', locationId],
     queryFn: () => getReturnDetail(locationId),
     enabled: !!locationId,
-    staleTime: 0,
-    refetchOnMount: 'always',
-    refetchOnWindowFocus: true,
-    refetchOnReconnect: true,
   });
 };
-
